fb28350d9258fe76e07acfce4f74182767ddd8cb
m_pur <m_pur@utb.cz>
2021-06-13 15:49:59 +0200

    fixed design in edit view

--- resources/views/reservations/edit.blade.php
+++ resources/views/reservations/edit.blade.php
@@ -31,25 +31,25 @@
             <div class="form-group mt-3">
             <label for="player_name">Jméno:</label>
             <input type="text"
-                class="form-control" name="player_name" id="player_name" aria-describedby="helpId" value="{{ $reservation['player_name'] }}">
+                class="form-control rounded" name="player_name" id="player_name" aria-describedby="helpId" value="{{ $reservation['player_name'] }}">
             </div>
 
             <div class="form-group mt-3">
             <label for="player_surname">Přijmení:</label>
             <input type="text"
-                class="form-control" name="player_surname" id="player_surname" aria-describedby="helpId" value="{{ $reservation['player_surname'] }}">
+                class="form-control rounded" name="player_surname" id="player_surname" aria-describedby="helpId" value="{{ $reservation['player_surname'] }}">
             </div>
 
             <div class="form-group mt-3">
                 <label for="email">Email:</label>
                 <input type="text"
-                    class="form-control" name="email" id="email" aria-describedby="helpId" value="{{ $reservation['email'] }}">
+                    class="form-control rounded" name="email" id="email" aria-describedby="helpId" value="{{ $reservation['email'] }}">
             </div>
 
             <div class="form-group mt-3">
                 <span>
                     <label for="court">Kurt:</label>
-                    <select name="court" id="court" value="{{ $reservation['court'] }}">
+                    <select name="court" id="court" value="{{ $reservation['court'] }}" class="text-dark rounded">
                         <option value="Kurt 1">Kurt 1</option>
                         <option value="Kurt 2">Kurt 2</option>
                     </select>
@@ -57,12 +57,12 @@
 
                 <span class="mx-5">
                     <label for="Date">Datum:</label>
-                    <input type="date" id="date" name="date" value="{{ $reservation['date'] }}">
+                    <input type="date" id="date" name="date" value="{{ $reservation['date'] }}" class="text-dark rounded">
                 </span>
 
                 <span class="mx-5">
                     <label for="start_time">Začátek:</label>
-                    <select name="start_time" id="start_time" value="{{ $reservation['start_time'] }}">
+                    <select name="start_time" id="start_time" value="{{ $reservation['start_time'] }}" class="text-dark rounded">
                         <option value="09:00:00">9:00</option>
                         <option value="10:00:00">10:00</option>
                         <option value="11:00:00">11:00</option>
@@ -79,7 +79,7 @@
 
                 <span class="mx-5">
                     <label for="end_time">Konec:</label>
-                    <select name="end_time" id="end_time" value="{{ $reservation['end_time'] }}">
+                    <select name="end_time" id="end_time" value="{{ $reservation['end_time'] }}" class="text-dark rounded">
                         <option value="10:00:00">10:00</option>
                         <option value="11:00:00">11:00</option>
                         <option value="12:00:00">12:00</option>
