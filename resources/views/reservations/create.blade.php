40631896be84af2c431ae81bc5f8cec26db51e66
m_pur <m_pur@utb.cz>
2021-06-13 15:45:59 +0200

    fixed design in create view

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -19,44 +19,44 @@
             }
         </script>
 
-        <form class="text-light" action="{{ route('reservations.store') }}" method="POST">
+        <form action="{{ route('reservations.store') }}" method="POST">
             @csrf
 
             <div class="form-group mt-3">
-            <label for="player_name">Jméno:</label>
+            <label for="player_name" class="text-light">Jméno:</label>
             <input type="text"
-                class="form-control" name="player_name" id="player_name" aria-describedby="helpId" placeholder="">
+                class="form-control rounded" name="player_name" id="player_name" aria-describedby="helpId" placeholder="">
             </div>
 
             <div class="form-group mt-3">
-            <label for="player_surname">Přijmení:</label>
+            <label for="player_surname" class="text-light">Přijmení:</label>
             <input type="text"
-                class="form-control" name="player_surname" id="player_surname" aria-describedby="helpId" placeholder="">
+                class="form-control rounded" name="player_surname" id="player_surname" aria-describedby="helpId" placeholder="">
             </div>
 
             <div class="form-group mt-3">
-                <label for="email">Email:</label>
+                <label for="email" class="text-light">Email:</label>
                 <input type="text"
-                    class="form-control" name="email" id="email" aria-describedby="helpId" placeholder="">
+                    class="form-control rounded" name="email" id="email" aria-describedby="helpId" placeholder="">
             </div>
 
             <div class="form-group mt-3">
                 <span>
-                    <label for="court">Kurt:</label>
-                    <select name="court" id="court">
+                    <label for="court" class="text-light">Kurt:</label>
+                    <select name="court" id="court" class="rounded">
                         <option value="Kurt 1">Kurt 1</option>
                         <option value="Kurt 2">Kurt 2</option>
                     </select>
                 </span>
 
                 <span class="mx-5">
-                    <label for="Date">Datum:</label>
-                    <input type="date" id="date" name="date">
+                    <label for="Date" class="text-light">Datum:</label>
+                    <input type="date" id="date" name="date" class="rounded">
                 </span>
 
                 <span class="mx-5">
-                    <label for="start_time">Začátek:</label>
-                    <select name="start_time" id="start_time">
+                    <label for="start_time" class="text-light">Začátek:</label>
+                    <select name="start_time" id="start_time" class="rounded">
                         <option value="09:00:00">9:00</option>
                         <option value="10:00:00">10:00</option>
                         <option value="11:00:00">11:00</option>
@@ -72,8 +72,8 @@
                 </span>
 
                 <span class="mx-5">
-                    <label for="end_time">Konec:</label>
-                    <select name="end_time" id="end_time">
+                    <label for="end_time" class="text-light">Konec:</label>
+                    <select name="end_time" id="end_time" class="rounded">
                         <option value="10:00:00">10:00</option>
                         <option value="11:00:00">11:00</option>
                         <option value="12:00:00">12:00</option>
--- resources/views/reservations/welcome.blade.php
+++ resources/views/reservations/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="h4 text-center text-light my-4">Přijďte si k nám zahrát</div>
         
-    <img src="storage/images/u1.png" alt="Tenis" class="img-fluid rounded mx-auto d-block">
+    <img src="storage/images/court.png" alt="Tenis" class="img-fluid rounded mx-auto d-block">
 
     <img src="storage/images/logo.png" alt="Logo" class="img-fluid rounded mx-auto d-block mt-4" style="overflow: hidden;">
 @endsection
